c7097916795bff54abd6b9b7af968a22f5c7808a
jinsung <treasure1209@naver.com>
2023-06-15 11:21:45 +0900

    Refactor:Todo.jsx 컴포넌트 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
+import Todo from "components/Todo";
 
 const App = () => {
   const [todos, setTodos] = useState([
@@ -93,23 +94,6 @@ const App = () => {
             />
           ))}
         </div>
-      </div>
-    </div>
-  );
-};
-
-const Todo = ({ item, clickRemoveButtonHandler, clickCompleteButtonHandler }) => {
-  return (
-    <div key={item.id} className="list-style">
-      <h2>{item.title}</h2>
-      <div>{item.content}</div>
-      <div className="bth-group">
-        <button className="remove-btn" onClick={() => clickRemoveButtonHandler(item.id)}>
-          삭제하기
-        </button>
-        <button className="isDone-btn" onClick={() => clickCompleteButtonHandler(item.id)}>
-          {item.isDone ? "취소" : "완료"}
-        </button>
       </div>
     </div>
   );
